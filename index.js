135dd6bb7c66a68b10d3ef8735ba7c2492282b60
Keith Cirkel <github@keithcirkel.co.uk>
2015-07-07 11:41:28 +0100

    Add chai-spies

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ global.chai = require('chai');
 global.chai
   .use(require('chai-as-promised'))
   .use(require('chai-things'))
+  .use(require('chai-spies'))
 ;
 global.should = global.chai.should();
